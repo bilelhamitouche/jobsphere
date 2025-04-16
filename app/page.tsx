4790703fb1446b409172894b97406b7c4a90a878
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-16 17:52:48 +0100

    mounted hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/ui/hero";
 import Navbar from "@/components/ui/navbar";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
