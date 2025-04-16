3031c3ca47392a882e95ca7be5b134ca8b80c305
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-16 17:52:08 +0100

    styled the not-found route

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,24 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 function NotFound() {
-  return <div className="text-red-500">Not found</div>;
+  return (
+    <div className="flex flex-col gap-4 justify-center items-center h-full">
+      <h2 className="text-6xl font-bold text-primary">404</h2>
+      <p className="text-xl">Page Not Found</p>
+      <p className="text-gray-600">
+        The page you are looking for doesn&apos;t exist or has been moved.
+      </p>
+      <div className="space-x-4">
+        <Button asChild>
+          <Link href="/">Return to Home</Link>
+        </Button>
+        <Button variant="outline" asChild>
+          <Link href="/jobs">Browse Jobs</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default NotFound;
