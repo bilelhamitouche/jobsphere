31c4310cf79f8adcffd1db6b4abcd45bcbc00430
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-16 17:52:40 +0100

    change font to inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
 });
 
@@ -20,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full light">
-      <body className={`${geistSans.className} h-full antialiased`}>
+      <body className={`${inter.className} h-full antialiased`}>
         {children}
         <Toaster richColors closeButton />
       </body>
