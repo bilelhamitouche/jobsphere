539a1b31ae8c42b3edf196817579bbb7150ba90c
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-17 08:56:20 +0100

    mount the popular categories component in page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/ui/hero";
 import Navbar from "@/components/ui/navbar";
+import PopularCategories from "@/components/ui/popular-categories";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <PopularCategories />
     </div>
   );
 }
