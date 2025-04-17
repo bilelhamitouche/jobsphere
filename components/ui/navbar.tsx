3ce67a7a9b574ca44f7dd6a38c52e02e22fcc343
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-17 08:55:43 +0100

    remove some styles and make it a little more responsive

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -15,13 +15,33 @@ import { authClient } from "@/lib/auth-client";
 function Navbar() {
   const { data: session } = authClient.useSession();
   return (
-    <header className="container flex justify-between items-center p-4 mx-auto max-w-6xl">
+    <header className="container flex justify-between items-center p-4 mx-auto">
       <Link href="/" className="flex gap-2 items-center">
         <BriefcaseBusiness size={25} className="text-primary" />
         <span className="text-2xl font-bold text-primary">JobSphere</span>
       </Link>
+      <nav className="hidden gap-4 items-center md:flex">
+        <Link
+          href="/jobs"
+          className="transition-colors duration-200 hover:text-primary"
+        >
+          Find Jobs
+        </Link>
+        <Link
+          href="/companies"
+          className="transition-colors duration-200 hover:text-primary"
+        >
+          Companies
+        </Link>
+        <Link
+          href="/about"
+          className="transition-colors duration-200 hover:text-primary"
+        >
+          About Us
+        </Link>
+      </nav>
       {!session?.user ? (
-        <div className="space-x-2">
+        <div className="hidden space-x-2 md:block">
           <Button variant="outline" asChild>
             <Link href="/signin">Sign In</Link>
           </Button>
