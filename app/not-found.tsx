8d8469cc80f60ae370ddece42cba01414a77a433
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-19 02:03:46 +0100

    made the not found page responsive

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 function NotFound() {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center h-full">
-      <h2 className="text-6xl font-bold text-primary">404</h2>
+    <div className="flex flex-col gap-4 justify-center items-center p-4 h-full text-center">
+      <h2 className="text-xl font-bold md:text-4xl lg:text-6xl text-primary">
+        404
+      </h2>
       <p className="text-xl">Page Not Found</p>
       <p className="text-gray-600">
         The page you are looking for doesn&apos;t exist or has been moved.
