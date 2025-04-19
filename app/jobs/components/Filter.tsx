e40aadc39e6eaf87817cc169a7defc0e40f530b4
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-19 02:04:11 +0100

    added a filter component for jobs

---
+++ app/jobs/components/Filter.tsx
@@ -0,0 +1,85 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import { Briefcase, Clock, MapPin } from "lucide-react";
+
+function Filter() {
+  return (
+    <Card className="col-start-1 col-end-3 row-start-2 row-end-3 md:col-end-2 min-w-xs">
+      <CardContent className="flex flex-col items-start space-y-4">
+        <CardTitle>Filters</CardTitle>
+        <form className="space-y-4 w-full">
+          <Separator />
+          <h2 className="flex gap-2 items-center">
+            <Briefcase size="18" />
+            <span>Job Type</span>
+          </h2>
+          <div className="flex gap-2">
+            <Checkbox id="full" name="full" value="full" />
+            <Label htmlFor="full">Full time</Label>
+          </div>
+          <div className="flex gap-2">
+            <Checkbox id="part" name="part" value="part" />
+            <Label htmlFor="part">Part time</Label>
+          </div>
+          <div className="flex gap-2">
+            <Checkbox id="contract" name="contract" value="contract" />
+            <Label htmlFor="contract">Contract</Label>
+          </div>
+          <div className="flex gap-2">
+            <Checkbox id="internship" name="internship" value="Internship" />
+            <Label htmlFor="internship">Internship</Label>
+          </div>
+          <Separator />
+          <h2 className="flex gap-2 items-center">
+            <MapPin size="18" />
+            <span>Location</span>
+          </h2>
+          <Select>
+            <SelectTrigger value="all" className="w-full">
+              All
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="all">All</SelectItem>
+              <SelectItem value="af">Africa</SelectItem>
+              <SelectItem value="us">America</SelectItem>
+              <SelectItem value="as">Asia</SelectItem>
+              <SelectItem value="au">Australia</SelectItem>
+              <SelectItem value="eu">Europe</SelectItem>
+              <SelectItem value="re">Remote</SelectItem>
+            </SelectContent>
+          </Select>
+          <Separator />
+          <h2 className="flex gap-2 items-center">
+            <Clock size="18" />
+            <span>Date Posted</span>
+          </h2>
+          <Select>
+            <SelectTrigger value="any" className="w-full">
+              Any Time
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="any">Any Time</SelectItem>
+              <SelectItem value="today">Today</SelectItem>
+              <SelectItem value="week">Past Week</SelectItem>
+              <SelectItem value="month">Past Month</SelectItem>
+            </SelectContent>
+          </Select>
+          <Button type="submit" className="mt-2 w-full">
+            Apply filters
+          </Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Filter;
