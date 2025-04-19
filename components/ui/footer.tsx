ad2c8a1cccb57dc61e987317db0453a0d7b17b6d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-19 01:58:28 +0100

    added a footer component

---
+++ components/ui/footer.tsx
@@ -0,0 +1,73 @@
+import { Separator } from "./separator";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
+import Link from "next/link";
+
+function Footer() {
+  return (
+    <footer className="p-4 border-t bg-primary-foreground border-t-gray-200">
+      <div className="grid grid-cols-1 gap-2 py-4 pb-8 md:grid-cols-4">
+        <div className="flex flex-col gap-4">
+          <h3 className="text-lg font-bold text-primary">JobSphere</h3>
+          <p className="text-sm text-gray-500">
+            Connecting the right talent with the right opportunities
+          </p>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h4 className="text-sm font-medium">For Job Seekers</h4>
+          <Link href="/jobs" className="text-sm text-gray-600">
+            Browse Jobs
+          </Link>
+          <Link href="/companies" className="text-sm text-gray-600">
+            Companies
+          </Link>
+          <Link href="/career-resources" className="text-sm text-gray-600">
+            Career Resources
+          </Link>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h4 className="text-sm font-medium">For Employers</h4>
+          <Link href="/jobs" className="text-sm text-gray-600">
+            Post a Jobs
+          </Link>
+          <Link href="/employer-resources" className="text-sm text-gray-600">
+            Employer Resources
+          </Link>
+        </div>
+        <div className="flex flex-col gap-4">
+          <h4 className="text-sm font-medium">Company</h4>
+          <Link href="/about" className="text-sm text-gray-600">
+            About Us
+          </Link>
+          <Link href="/contact" className="text-sm text-gray-500">
+            Contact
+          </Link>
+          <Link href="/privacy" className="text-sm text-gray-500">
+            Privacy Policy
+          </Link>
+        </div>
+      </div>
+      <Separator />
+      <div className="flex justify-between items-center py-4">
+        <p className="text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} JobSphere. All rights reserved.
+        </p>
+        <div className="flex gap-4 items-center">
+          <Link href="https://twitter.com/jobsphere">
+            <FaTwitter
+              size="20"
+              className="text-gray-500 transition-colors hover:text-gray-600"
+            />
+          </Link>
+          <Link href="https://linkedin.com/jobsphere">
+            <FaLinkedin
+              size="20"
+              className="text-gray-500 transition-colors hover:text-gray-600"
+            />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
