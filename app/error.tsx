d9581212fd177b103f16bc54e759687d4214243e
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-19 02:03:54 +0100

    made the error route responsive

--- app/error.tsx
+++ app/error.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 function Error() {
   return (
-    <div className="flex flex-col gap-4 justify-center items-center h-full">
-      <h1 className="text-6xl font-bold text-red-500">
+    <div className="flex flex-col gap-4 justify-center items-center p-4 h-full text-center">
+      <h1 className="text-3xl font-bold text-red-500 md:text-4xl">
         OOPS!! Something went wrong
       </h1>
       <p className="text-gray-500">
