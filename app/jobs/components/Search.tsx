69370c5ac1c10169114d586af4086e974f6c89e4
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-19 02:04:20 +0100

    added a search component for jobs

---
+++ app/jobs/components/Search.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+
+function Search() {
+  return (
+    <Card className="col-span-2 row-start-1 row-end-2">
+      <CardContent className="flex flex-col gap-4">
+        <form className="flex gap-2 items-center">
+          <Input type="text" placeholder="Search for jobs" />
+          <Button type="submit">Search</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default Search;
