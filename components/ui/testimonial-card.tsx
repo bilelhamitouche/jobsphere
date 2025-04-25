f9f3e084ff28a97dac2ab1af21884433f5983749
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-25 12:23:09 +0100

    added testimonial card

---
+++ components/ui/testimonial-card.tsx
@@ -0,0 +1,33 @@
+import { Avatar, AvatarImage } from "./avatar";
+import { Card, CardContent, CardFooter, CardHeader } from "./card";
+
+interface TestimonialCardProps {
+  avatarImage: string;
+  testimonial: string;
+  name: string;
+  job: string;
+}
+
+function TestimonialCard({
+  avatarImage,
+  testimonial,
+  name,
+  job,
+}: TestimonialCardProps) {
+  return (
+    <Card className="text-left">
+      <CardHeader>
+        <Avatar>
+          <AvatarImage src={avatarImage} alt={`${name} image`} />
+        </Avatar>
+      </CardHeader>
+      <CardContent>{testimonial}</CardContent>
+      <CardFooter className="flex flex-col gap-1 items-start">
+        <span className="font-medium">{name}</span>
+        <span className="text-sm text-gray-500">{job}</span>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export default TestimonialCard;
