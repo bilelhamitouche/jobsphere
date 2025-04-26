9e3cdc9db4809f7a8d9458e09f284271c440a082
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-04-26 03:38:38 +0100

    added nuqs adapter for next app router

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full light">
       <body className={`${inter.className} h-full antialiased`}>
-        {children}
+        <NuqsAdapter>{children}</NuqsAdapter>
         <Toaster richColors closeButton theme="light" />
       </body>
     </html>
