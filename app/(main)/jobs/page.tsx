a2260574a8085311dc3e8cea95b9f8a7743672ee
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-02 18:19:54 +0100

    mount the components in the page

---
+++ app/(main)/jobs/page.tsx
@@ -0,0 +1,16 @@
+import Filter from "./components/Filter";
+import JobsList from "./components/JobsList";
+import Search from "./components/Search";
+
+export default function Jobs() {
+  return (
+    <div className="p-8 space-y-8 h-full bg-primary-foreground">
+      <h1 className="text-2xl font-bold">Browse Jobs</h1>
+      <div className="grid gap-4 grid-rows-[auto_auto_1fr] grid-cols-[auto_1fr]">
+        <Search />
+        <Filter />
+        <JobsList />
+      </div>
+    </div>
+  );
+}
