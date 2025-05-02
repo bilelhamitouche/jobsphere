6653ece59d8fad130b06f3d50059e429990b50d8
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-02 18:19:45 +0100

    added filter, search and joblisting components for the jobs page

---
+++ app/(main)/jobs/components/JobsList.tsx
@@ -0,0 +1,18 @@
+import JobCard from "./JobCard";
+
+function JobsList() {
+  return (
+    <ul className="flex flex-col col-start-1 col-end-3 row-start-3 row-end-4 gap-4 w-full md:col-start-2 md:row-start-2 md:row-end-3">
+      <JobCard
+        companyImageUrl={null}
+        company="TechNet"
+        position="Fullstack Developer"
+        type="full"
+        experienceLevel="entry"
+        location="Remote"
+      />
+    </ul>
+  );
+}
+
+export default JobsList;
