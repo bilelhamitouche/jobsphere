545135e2556264aa4f579c10e0ffbd8b6c446949
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-08 15:50:31 +0100

    add applications route

---
+++ app/recruiter/applications/page.tsx
@@ -0,0 +1,7 @@
+export default function RecruiterApplications() {
+  return (
+    <div className="p-4 w-full h-full">
+      <h2 className="text-2xl font-semibold">All Applications</h2>
+    </div>
+  );
+}
