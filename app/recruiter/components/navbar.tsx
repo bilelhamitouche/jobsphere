5b61eacbbf693ee68e0d79fc5bff96d3efb7d101
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-09 00:58:13 +0100

    check for path to conditionally render search bar

--- app/recruiter/components/navbar.tsx
+++ app/recruiter/components/navbar.tsx
@@ -9,7 +9,7 @@ export default function Navbar() {
   return (
     <header className="flex gap-4 items-center p-4 w-full bg-white border-b border-gray-200 h-18">
       <SidebarTrigger />
-      {pathname !== "/recruiter" && (
+      {pathname !== "/recruiter" && pathname !== "/recruiter/company" && (
         <Input placeholder="Search" className="w-xs" />
       )}
     </header>
