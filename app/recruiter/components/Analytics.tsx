97481b386c347376474eb8951a1efc583e41919d
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-12 00:01:08 +0100

    add components for analytics, recent jobs, total applications, total Jobs

---
+++ app/recruiter/components/Analytics.tsx
@@ -0,0 +1,9 @@
+import TotalJobsCard from "./TotalJobsCard";
+
+export default function Analytics() {
+  return (
+    <div className="grid grid-cols-1 p-4 w-full md:grid-cols-2 lg:grid-cols-3">
+      <TotalJobsCard />
+    </div>
+  );
+}
