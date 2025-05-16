fd91625dbdc1d6731a5842fa680b49944ccc7759
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-16 03:10:03 +0100

    added fetching company info

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,2 +1,17 @@
 import "server-only";
+import { company, db } from "./drizzle";
+import { DrizzleError, eq } from "drizzle-orm";
+import { isRecruiterAuthenticated } from "@/actions/auth";
 export async function getJobListing() {}
+
+export async function getCompanyInfoById(recruiterId: string) {
+  await isRecruiterAuthenticated()
+  try {
+    const companyInfo = await db.selectDistinct().from(company).where(eq(company.recruiterId, recruiterId));
+    return companyInfo;
+  } catch (err) {
+    if(err instanceof DrizzleError) {
+      throw new Error("Database Error")
+    }
+  }
+}
