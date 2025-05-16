ef4270d4ab51fa62fe89b60550ebc5d80d93d393
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-16 03:10:19 +0100

    add getCompany info function to fetch on client

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function getCompanyInfo() {
+  const response = await fetch("/api/recruiter/company");
+  const companyInfo = await response.json();
+  return companyInfo;
+}
