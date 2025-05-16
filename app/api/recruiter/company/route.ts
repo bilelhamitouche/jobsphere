038188a3a008e21d40ada2411fad2d3cc156cf8a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-16 03:06:58 +0100

    api route for company information

---
+++ app/api/recruiter/company/route.ts
@@ -0,0 +1,9 @@
+import { getUserInfo } from "@/actions/auth";
+import { getCompanyInfoById } from "@/lib/queries";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const user = await getUserInfo();
+  const companyInfo = await getCompanyInfoById(user?.id as string)
+  return NextResponse.json(companyInfo, { status: 200 })
+}
