0e4403a32bf32b0ae67dc11c551f630fcff63c2a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-17 03:32:15 +0100

    formatting

--- app/api/recruiter/company/route.ts
+++ app/api/recruiter/company/route.ts
@@ -5,5 +5,5 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const user = await getUserInfo();
   const companyInfo = await getCompanyInfoById(user?.id as string);
-  return NextResponse.json(companyInfo, { status: 200 })
+  return NextResponse.json(companyInfo, { status: 200 });
 }
