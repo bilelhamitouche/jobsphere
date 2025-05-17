643e4105a177d834523c1b8fbbad35fa3372d39a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-17 03:32:32 +0100

    formatting

--- lib/queries.ts
+++ lib/queries.ts
@@ -5,13 +5,49 @@ import { isRecruiterAuthenticated } from "@/actions/auth";
 export async function getJobListing() {}
 
 export async function getCompanyInfoById(recruiterId: string) {
-  await isRecruiterAuthenticated()
+  await isRecruiterAuthenticated();
   try {
-    const companyInfo = await db.selectDistinct().from(company).where(eq(company.recruiterId, recruiterId));
+    const companyInfo = await db
+      .select()
+      .from(company)
+      .where(eq(company.recruiterId, recruiterId));
     return companyInfo;
   } catch (err) {
-    if(err instanceof DrizzleError) {
-      throw new Error("Database Error")
+    if (err instanceof DrizzleError) {
+      throw new Error("Database Error");
+    }
+  }
+}
+
+export async function createCompany(
+  name: string,
+  email: string,
+  about: string | null,
+  foundationYear: number,
+  headquarters: string | null,
+  website: string | null,
+  recruiterId: string,
+  industryId: string,
+) {
+  await isRecruiterAuthenticated();
+  try {
+    const newCompany = await db
+      .insert(company)
+      .values({
+        name,
+        email,
+        about,
+        foundationYear,
+        headquarters,
+        website,
+        recruiterId,
+        industryId,
+      })
+      .returning();
+    console.log(newCompany);
+  } catch (err) {
+    if (err instanceof DrizzleError) {
+      throw new Error("Database Error");
     }
   }
 }
