963523618699b17c01a9d01943465b68cb293564
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-17 03:19:46 +0100

    added company server actions (get, post...)

--- actions/company.ts
+++ actions/company.ts
@@ -1,8 +1,28 @@
 "use server";
-import { getCompanyInfoById } from "@/lib/queries";
 import "server-only";
+import { companyInfoSchema } from "@/lib/zod";
+import { getCompanyInfoById } from "@/lib/queries";
 
 export async function getCompanyInfoAction(recruiterId: string) {
   const companyInfo = await getCompanyInfoById(recruiterId);
   return companyInfo;
+}
+
+export async function createCompany(formData: FormData) {
+  const name = formData.get('name') as string;
+  const email = formData.get('email') as string;
+  const about = formData.get('about') as string;
+  const foundationYear = formData.get('foundation_year');
+  const website = formData.get('website') as string;
+  const industry = formData.get('industry') as string;
+  const logoUrl = formData.get("logo_url") as string;
+  const recruiterId = formData.get("recruiterId") as string;
+  console.log(name, email, about, foundationYear, website, industry, logoUrl, recruiterId)
+  
+  const result = companyInfoSchema.safeParse({ name, email, about, foundationYear, website, industry, logoUrl })
+  if(!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors
+    }
+  }
 }
