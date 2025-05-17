cfe305e403d8caf6bcc98439a99f306672e003ed
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-17 03:32:09 +0100

    formatting

--- actions/company.ts
+++ actions/company.ts
@@ -9,20 +9,37 @@ export async function getCompanyInfoAction(recruiterId: string) {
 }
 
 export async function createCompany(formData: FormData) {
-  const name = formData.get('name') as string;
-  const email = formData.get('email') as string;
-  const about = formData.get('about') as string;
-  const foundationYear = formData.get('foundation_year');
-  const website = formData.get('website') as string;
-  const industry = formData.get('industry') as string;
+  const name = formData.get("name") as string;
+  const email = formData.get("email") as string;
+  const about = formData.get("about") as string;
+  const foundationYear = formData.get("foundation_year");
+  const website = formData.get("website") as string;
+  const industry = formData.get("industry") as string;
   const logoUrl = formData.get("logo_url") as string;
   const recruiterId = formData.get("recruiterId") as string;
-  console.log(name, email, about, foundationYear, website, industry, logoUrl, recruiterId)
-  
-  const result = companyInfoSchema.safeParse({ name, email, about, foundationYear, website, industry, logoUrl })
-  if(!result.success) {
+  console.log(
+    name,
+    email,
+    about,
+    foundationYear,
+    website,
+    industry,
+    logoUrl,
+    recruiterId,
+  );
+
+  const result = companyInfoSchema.safeParse({
+    name,
+    email,
+    about,
+    foundationYear,
+    website,
+    industry,
+    logoUrl,
+  });
+  if (!result.success) {
     return {
-      errors: result.error.flatten().fieldErrors
-    }
+      errors: result.error.flatten().fieldErrors,
+    };
   }
 }
