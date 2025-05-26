7e7ad42c426682dd3a64789415095dc31e8ba3ae
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-26 17:38:34 +0100

    remove flash(shows company-info form before going to recruiter dashboard)

--- app/company-info/page.tsx
+++ app/company-info/page.tsx
@@ -87,178 +87,185 @@ export default function CompanyInfo() {
       </div>
     );
   return (
-    <div className="flex flex-col gap-4 justify-center items-start p-4 w-full h-full md:p-8">
-      <Button variant="link" className="font-semibold text-gray-800" asChild>
-        <Link href="/">
-          <ArrowLeft size="10" />
-          <span>Back to home</span>
-        </Link>
-      </Button>
-      <Card className="w-full">
-        <CardHeader>
-          <CardTitle className="text-xl font-bold">Company Info</CardTitle>
-          <CardDescription>Enter your company info</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              className="grid gap-4 md:grid-cols-2"
-              onSubmit={form.handleSubmit(
-                async (data: z.infer<typeof companyInfoSchema>) => {
-                  setIsLoading(true);
-                  const formData = new FormData();
-                  formData.append("name", data.name);
-                  formData.append("email", data.email);
-                  formData.append("about", data.about as string);
-                  formData.append("foundation_year", data.foundation_year);
-                  formData.append("website", data.website as string);
-                  formData.append("headquarters", data.headquarters as string);
-                  formData.append("industry", data.industry);
-                  formData.append("logo_url", data.logo_url as string);
-                  formData.append("recruiter_id", session?.user.id as string);
-                  try {
-                    const result = await createCompanyAction(formData);
-                  } catch (err) {
-                    console.log(err);
-                  } finally {
-                    setIsLoading(false);
-                  }
-                },
-              )}
-            >
-              <FormField
-                name="name"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Name</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
+    !isPending &&
+    !isSessionPending &&
+    !companyInfo && (
+      <div className="flex flex-col gap-4 justify-center items-start p-4 w-full h-full md:p-8">
+        <Button variant="link" className="font-semibold text-gray-800" asChild>
+          <Link href="/">
+            <ArrowLeft size="10" />
+            <span>Back to home</span>
+          </Link>
+        </Button>
+        <Card className="w-full">
+          <CardHeader>
+            <CardTitle className="text-xl font-bold">Company Info</CardTitle>
+            <CardDescription>Enter your company info</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form
+                className="grid gap-4 md:grid-cols-2"
+                onSubmit={form.handleSubmit(
+                  async (data: z.infer<typeof companyInfoSchema>) => {
+                    setIsLoading(true);
+                    const formData = new FormData();
+                    formData.append("name", data.name);
+                    formData.append("email", data.email);
+                    formData.append("about", data.about as string);
+                    formData.append("foundation_year", data.foundation_year);
+                    formData.append("website", data.website as string);
+                    formData.append(
+                      "headquarters",
+                      data.headquarters as string,
+                    );
+                    formData.append("industry", data.industry);
+                    formData.append("logo_url", data.logo_url as string);
+                    formData.append("recruiter_id", session?.user.id as string);
+                    try {
+                      const result = await createCompanyAction(formData);
+                    } catch (err) {
+                      console.log(err);
+                    } finally {
+                      setIsLoading(false);
+                    }
+                  },
                 )}
-              />
-              <FormField
-                name="email"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input type="email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="headquarters"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Headquarters</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="industry"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Industry</FormLabel>
-                    <Select onValueChange={field.onChange}>
-                      <FormControl>
-                        <SelectTrigger className="w-full">
-                          <SelectValue placeholder="Enter a industry" />
-                        </SelectTrigger>
-                      </FormControl>
-                      <SelectContent>
-                        {selectOptions.map((option, index) => (
-                          <SelectItem key={index} value={option}>
-                            {option}
-                          </SelectItem>
-                        ))}
-                      </SelectContent>
-                    </Select>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="foundation_year"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Foundation Year</FormLabel>
-                    <FormControl>
-                      <Input type="number" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="website"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Website</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="logo_url"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem className="md:col-span-2">
-                    <FormLabel>Logo Url</FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                name="about"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem className="md:col-span-2">
-                    <FormLabel>About</FormLabel>
-                    <FormControl>
-                      <Textarea {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button
-                type="submit"
-                disabled={isLoading}
-                className="md:col-span-2"
               >
-                {isLoading ? (
-                  <div className="flex gap-2 items-center">
-                    <Loader2 className="animate-spin"></Loader2>
-                    <span>Please wait</span>
-                  </div>
-                ) : (
-                  <span>Save</span>
-                )}
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
+                <FormField
+                  name="name"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="email"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input type="email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="headquarters"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Headquarters</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="industry"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Industry</FormLabel>
+                      <Select onValueChange={field.onChange}>
+                        <FormControl>
+                          <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Enter a industry" />
+                          </SelectTrigger>
+                        </FormControl>
+                        <SelectContent>
+                          {selectOptions.map((option, index) => (
+                            <SelectItem key={index} value={option}>
+                              {option}
+                            </SelectItem>
+                          ))}
+                        </SelectContent>
+                      </Select>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="foundation_year"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Foundation Year</FormLabel>
+                      <FormControl>
+                        <Input type="number" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="website"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Website</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="logo_url"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem className="md:col-span-2">
+                      <FormLabel>Logo Url</FormLabel>
+                      <FormControl>
+                        <Input {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  name="about"
+                  control={form.control}
+                  render={({ field }) => (
+                    <FormItem className="md:col-span-2">
+                      <FormLabel>About</FormLabel>
+                      <FormControl>
+                        <Textarea {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button
+                  type="submit"
+                  disabled={isLoading}
+                  className="md:col-span-2"
+                >
+                  {isLoading ? (
+                    <div className="flex gap-2 items-center">
+                      <Loader2 className="animate-spin"></Loader2>
+                      <span>Please wait</span>
+                    </div>
+                  ) : (
+                    <span>Save</span>
+                  )}
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+        </Card>
+      </div>
+    )
   );
 }
