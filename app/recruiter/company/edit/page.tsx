b5fcb6d2123c9687accc6799646695c10f60991a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-31 03:58:13 +0100

    added edit company form

---
+++ app/recruiter/company/edit/page.tsx
@@ -0,0 +1,15 @@
+import { getUserInfo } from "@/actions/auth";
+import EditCompanyInfoForm from "../components/EditCompanyInfoForm";
+import { getCompanyInfoById } from "@/lib/queries";
+
+export default async function EditCompanyInfo() {
+  const user = await getUserInfo();
+  const companyInfo = await getCompanyInfoById(user?.id as string);
+  if (!companyInfo) return;
+  return (
+    <div className="p-6 space-y-4 w-full h-full bg-primary-foreground">
+      <h2 className="text-3xl font-bold">Edit Company Information</h2>
+      <EditCompanyInfoForm companyInfo={companyInfo} />
+    </div>
+  );
+}
