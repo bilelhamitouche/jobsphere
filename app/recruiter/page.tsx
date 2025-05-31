55cc0b30d7cc46bf9241a3cd5ef73112859f3ffa
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-31 03:59:43 +0100

    change title size

--- app/recruiter/page.tsx
+++ app/recruiter/page.tsx
@@ -4,8 +4,8 @@ async function Recruiter() {
   await isRecruiterAuthenticated();
   return (
     <div className="grid w-full h-full grid-rows-[auto_1fr] bg-primary-foreground">
-      <div className="p-4 h-full">
-        <h2 className="text-2xl font-semibold">Dashboard</h2>
+      <div className="p-6 space-y-4 h-full">
+        <h2 className="text-3xl font-semibold">Dashboard</h2>
       </div>
     </div>
   );
