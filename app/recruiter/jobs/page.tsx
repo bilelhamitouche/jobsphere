6a2c634c8fc6d7ab40cdbdd2f3526e3112094594
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-05-31 03:59:33 +0100

    added icon and change title size

--- app/recruiter/jobs/page.tsx
+++ app/recruiter/jobs/page.tsx
@@ -5,15 +5,19 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { PlusCircle } from "lucide-react";
 import Link from "next/link";
 
 export default function RecruiterJobs() {
   return (
-    <div className="p-4 space-y-4 w-full h-full">
+    <div className="p-6 space-y-4 w-full h-full">
       <div className="flex justify-between items-center">
-        <h2 className="text-2xl font-semibold">All Jobs</h2>
+        <h2 className="text-3xl font-semibold">All Jobs</h2>
         <Button asChild>
-          <Link href="/recruiter/jobs/create">Post New Job</Link>
+          <Link href="/recruiter/jobs/create">
+            <PlusCircle />
+            <span>Post New Job</span>
+          </Link>
         </Button>
       </div>
       <Card>
