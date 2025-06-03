82c64f152f3f926f02912ecb22891b1907760698
bilelhamitouche4@gmail.com <bilel@gentoo.homenetwork>
2025-06-03 01:00:25 +0100

    add job post form

---
+++ app/recruiter/jobs/post/components/PostJobForm.tsx
@@ -0,0 +1,108 @@
+"use client";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { z } from "zod";
+import { jobListingSchema, jobType, jobExperienceLevel } from "@/lib/zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+
+export default function PostJobForm() {
+  const typeOptions = jobType.options;
+  const experienceLevelOptions = jobExperienceLevel.options;
+  const form = useForm<z.infer<typeof jobListingSchema>>({
+    resolver: zodResolver(jobListingSchema),
+    defaultValues: {
+      position: "",
+      type: "full",
+      description: "",
+      experience_level: "none",
+      location: "",
+    }
+  });
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="text-xl">Job Details</CardTitle>
+        <CardDescription>Fill in the form to post a job</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form className="grid grid-cols-1 md:grid-cols-2 gap-4 space-y-2">
+            <FormField name="position" control={form.control} render={({ field }) => (
+            <FormItem>
+                <FormLabel>Position</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <FormField name="location" control={form.control} render={({ field }) => (
+            <FormItem>
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <FormField name="type" control={form.control} render={({ field }) => (
+            <FormItem>
+                <FormLabel>Type</FormLabel>
+                <Select>
+                  <FormControl>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select the type" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {typeOptions.map((option, index) => (
+                      <SelectItem key={index} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <FormField name="experience_level" control={form.control} render={({ field }) => (
+            <FormItem>
+                <FormLabel>Experience Level</FormLabel>
+                <Select>
+                  <FormControl>
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select the experience level" />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {experienceLevelOptions.map((option, index) => (
+                      <SelectItem key={index} value={option}>
+                        {option}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <FormField name="description" control={form.control} render={({ field }) => (
+            <FormItem className="md:col-span-2">
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea {...field}></Textarea>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )} />
+            <Button className="md:col-span-2">Save</Button>
+          </form>
+        </Form>        
+      </CardContent>
+    </Card>
+  )
+}
