e651e0d63d798edf9571c495a28cd85f20e623a6
bilelhamitouche4@gmail.com <bilel@gentoo.homenetwork>
2025-06-03 01:00:46 +0100

    change a few styles and put the form into it's own component

--- app/recruiter/jobs/page.tsx
+++ app/recruiter/jobs/page.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 import { PlusCircle } from "lucide-react";
 import Link from "next/link";
@@ -14,17 +11,13 @@ export default function RecruiterJobs() {
       <div className="flex justify-between items-center">
         <h2 className="text-3xl font-semibold">All Jobs</h2>
         <Button asChild>
-          <Link href="/recruiter/jobs/create">
+          <Link href="/recruiter/jobs/post">
             <PlusCircle />
             <span>Post New Job</span>
           </Link>
         </Button>
       </div>
       <Card>
-        <CardHeader>
-          <CardTitle>All Jobs</CardTitle>
-          <CardDescription>All jobs you have posted</CardDescription>
-        </CardHeader>
       </Card>
     </div>
   );
--- app/recruiter/jobs/post/page.tsx
+++ app/recruiter/jobs/post/page.tsx
@@ -1,28 +1,10 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import PostJobForm from "./components/PostJobForm";
 
 export default function PostJob() {
-  const form = useForm({
-    resolver: zodResolver(),
-    defaultValues: {},
-  });
   return (
-    <div className="p-4 w-full h-full">
-      <h2 className="text-2xl font-semibold">Post Job</h2>
-      <Card>
-        <CardHeader>
-          <CardTitle>Post Job</CardTitle>
-          <CardDescription>Fill out the form to post a new job</CardDescription>
-        </CardHeader>
-        <CardContent></CardContent>
-      </Card>
+    <div className="p-8 space-y-4 w-full h-full">
+      <h2 className="text-3xl font-semibold">Post Job</h2>
+      <PostJobForm />
     </div>
   );
 }
