fb3624a105fe068a4f0c0d794678c9d94d854cbd
bilelhamitouche4@gmail.com <bilel@gentoo.homenetwork>
2025-06-04 02:49:37 +0100

    added a few jobListing queries

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,11 +1,62 @@
 import "server-only";
-import { company, db } from "./drizzle";
+import { company, db, jobListing } from "./drizzle";
 import { DrizzleError, eq } from "drizzle-orm";
-import { isRecruiterAuthenticated } from "@/actions/auth";
+import { isAuthenticated, isRecruiterAuthenticated } from "@/actions/auth";
 import { z } from "zod";
-import { companyIndustry } from "./zod";
+import { companyIndustry, jobExperienceLevel, jobType } from "./zod";
 
-export async function getJobListing() {}
+export async function getJobListings() {
+  await isAuthenticated();
+  try {
+    const jobListings = await db.select().from(jobListing);
+    return jobListings;
+  } catch (err) {
+    if (err instanceof DrizzleError) {
+      throw new Error("Database Error");
+    }
+  }
+}
+
+export async function getJobListingsById(recruiterId: string) {
+  await isRecruiterAuthenticated();
+  try {
+  } catch (err) {
+    if (err instanceof DrizzleError) {
+      throw new Error("Database Error");
+    }
+  }
+}
+
+export async function createJobListing(
+  position: string,
+  location: string | null,
+  type: z.infer<typeof jobType>,
+  experience_level: z.infer<typeof jobExperienceLevel>,
+  description: string | null,
+  recruiterId: string,
+) {
+  await isRecruiterAuthenticated();
+  try {
+    const companyId = await db
+      .select({ id: company.id })
+      .from(company)
+      .where(eq(company.recruiterId, recruiterId));
+    console.log("Hello world");
+    console.log(companyId);
+    const newJobListing = await db
+      .insert(jobListing)
+      .values({
+        position,
+        description,
+        type,
+        experienceLevel: experience_level,
+        location,
+        companyId,
+      })
+      .returning();
+    console.log(newJobListing);
+  } catch (err) {}
+}
 
 export async function getCompanyInfoById(recruiterId: string) {
   await isRecruiterAuthenticated();
@@ -64,16 +115,6 @@ export async function updateCompany(
   industry: z.infer<typeof companyIndustry> | undefined,
 ) {
   await isRecruiterAuthenticated();
-  const updatedData: {
-    name?: string;
-    about?: string;
-    email?: string;
-    foundationYear?: number;
-    headquarters?: string;
-    website?: string;
-    logoUrl?: string;
-    industry?: z.infer<typeof companyIndustry>;
-  } = {};
   try {
     await db
       .update(company)
