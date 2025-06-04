b90b416572de5b3d82bac0fe6072c9d0c60bd42e
bilelhamitouche4@gmail.com <bilel@gentoo.homenetwork>
2025-06-04 02:49:51 +0100

    add recruiterId to the form

--- app/recruiter/jobs/post/page.tsx
+++ app/recruiter/jobs/post/page.tsx
@@ -1,10 +1,12 @@
+import { getUserInfo } from "@/actions/auth";
 import PostJobForm from "./components/PostJobForm";
 
-export default function PostJob() {
+export default async function PostJob() {
+  const user = await getUserInfo();
   return (
     <div className="p-8 space-y-4 w-full h-full">
       <h2 className="text-3xl font-semibold">Post Job</h2>
-      <PostJobForm />
+      <PostJobForm recruiterId={user?.id as string} />
     </div>
   );
 }
