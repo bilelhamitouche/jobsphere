b6dfa5151d0e82f5f4ef29fd26c1ef5a0fcfcbd1
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-09 19:11:51 +0100

    remove search bar and change to server component

--- app/recruiter/components/navbar.tsx
+++ app/recruiter/components/navbar.tsx
@@ -1,17 +1,9 @@
-"use client";
-
-import { Input } from "@/components/ui/input";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { usePathname } from "next/navigation";
 
 export default function Navbar() {
-  const pathname = usePathname();
   return (
     <header className="flex gap-4 items-center p-4 w-full bg-white border-b border-gray-200 h-18">
       <SidebarTrigger />
-      {pathname !== "/recruiter" && pathname !== "/recruiter/company" && (
-        <Input placeholder="Search" className="w-xs" />
-      )}
     </header>
   );
 }
