77faf925d227fc61234db148a0bc18d8daad9dea
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-12 14:03:38 +0100

    fetch jobListings

--- app/(main)/jobs/components/JobsList.tsx
+++ app/(main)/jobs/components/JobsList.tsx
@@ -1,6 +1,10 @@
 import JobCard from "./JobCard";
+import { getJobListings } from "@/lib/queries";
 
 export default function JobsList() {
+export default async function JobsList() {
+  const jobs = await getJobListings();
+  if (!jobs || jobs.length === 0) return <div>No jobs here</div>;
   return (
     <ul className="flex flex-col col-start-1 col-end-3 row-start-3 row-end-4 gap-4 w-full md:col-start-2 md:row-start-2 md:row-end-3">
       <JobCard
