10121a543fd635fcec62e6ef49d6ce1b5caa8025
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-14 01:36:08 +0100

    add spacing and make the title bigger

--- app/(main)/companies/page.tsx
+++ app/(main)/companies/page.tsx
@@ -1,11 +1,13 @@
+import CompaniesList from "./components/CompaniesList";
 import Search from "./components/Search";
 
 function Companies() {
   return (
     <div className="p-8 space-y-8 bg-primary-foreground">
-      <h2 className="text-2xl font-bold">Browse Companies</h2>
-      <div className="">
+      <h2 className="text-3xl font-bold">Browse Companies</h2>
+      <div className="space-y-8">
         <Search />
+        <CompaniesList />
       </div>
     </div>
   );
