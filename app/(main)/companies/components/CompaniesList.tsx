71d61681215bef59e4f41fa5d7cb9a291c9086bb
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-14 01:35:35 +0100

    fetch data and display companies in grid

--- app/(main)/companies/components/CompaniesList.tsx
+++ app/(main)/companies/components/CompaniesList.tsx
@@ -1,5 +1,29 @@
-function CompaniesList() {
-  return <div className="flex flex-col gap-4"></div>;
-}
+import { getCompanies } from "@/lib/queries";
+import CompanyCard from "./CompanyCard";
 
-export default CompaniesList;
+export default async function CompaniesList() {
+  const companies = await getCompanies();
+  if (!companies)
+    return (
+      <div className="p-8">
+        <span className="text-lg font-semibold text-center text-gray-500">
+          No companies found
+        </span>
+      </div>
+    );
+  return (
+    <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      {companies.map((company) => (
+        <CompanyCard
+          key={company.id}
+          id={company.id}
+          name={company.name}
+          imageUrl={company.logoUrl}
+          industry={company.industry}
+          foundationYear={company.foundationYear}
+          website={company.website}
+        />
+      ))}
+    </ul>
+  );
+}
