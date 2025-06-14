03a174d7b1627edcc8e3d8e7450028b46c74ba56
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-14 01:35:48 +0100

    finish company cards and add website field

--- app/(main)/companies/components/CompanyCard.tsx
+++ app/(main)/companies/components/CompanyCard.tsx
@@ -1,12 +1,13 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 interface CompanyCardProps {
   id: string;
   name: string;
-  imageUrl: string;
+  imageUrl: string | null;
   industry: string;
   foundationYear: number;
-  website: string;
+  website: string | null;
 }
 
 function CompanyCard({
@@ -18,10 +19,27 @@ function CompanyCard({
   website,
 }: CompanyCardProps) {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle></CardTitle>
+    <Card className="w-full">
+      <CardHeader className="flex flex-col gap-4 items-start">
+        <CardTitle className="flex gap-4 items-center">
+          {imageUrl ? (
+            <Avatar>
+              <AvatarImage
+                src={imageUrl}
+                alt="${company} url"
+                width="18"
+                height="18"
+              />
+            </Avatar>
+          ) : (
+            <div className="flex justify-center items-center text-xl size-18 bg-primary-foreground">
+              {name.toUpperCase()[0]}
+            </div>
+          )}
+          <span className="text-lg font-medium">{name}</span>
+        </CardTitle>
       </CardHeader>
+      <CardContent></CardContent>
     </Card>
   );
 }
