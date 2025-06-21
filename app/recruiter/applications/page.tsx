c22e5c9f9ec9f437e99f6ce7538f6ef74876b099
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-21 21:19:55 +0100

    get applications and display them in a data table

--- app/recruiter/applications/page.tsx
+++ app/recruiter/applications/page.tsx
@@ -1,19 +1,19 @@
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "./lib/columns";
+import { getUserInfo } from "@/actions/auth";
+import { getJobApplicationsByRecruiterId } from "@/lib/queries";
 
-export default function RecruiterApplications() {
+export default async function RecruiterApplications() {
+  const user = await getUserInfo();
+  const jobApplications = await getJobApplicationsByRecruiterId(user.id);
   return (
     <div className="p-6 space-y-4 w-full h-full">
       <h2 className="text-3xl font-semibold">All Applications</h2>
       <Card>
-        <CardHeader>
-          <CardTitle>All Applications</CardTitle>
-          <CardDescription>All Applications to jobs you posted</CardDescription>
-        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={jobApplications} />
+        </CardContent>
       </Card>
     </div>
   );
