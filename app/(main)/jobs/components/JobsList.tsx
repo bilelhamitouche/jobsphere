355a6a9ff637f14a4fd2d88e3dd4c66b0839a190
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-06-25 20:57:16 +0100

    added a few style improvements

--- app/(main)/jobs/components/JobsList.tsx
+++ app/(main)/jobs/components/JobsList.tsx
@@ -5,7 +5,9 @@ export default async function JobsList() {
   const jobs = await getJobListings();
   if (!jobs || jobs.length === 0)
     return (
-      <div className="p-8 text-lg font-medium text-gray-700">No jobs found</div>
+      <div className="py-16 text-lg font-medium text-center text-gray-700">
+        No jobs found
+      </div>
     );
   return (
     <ul className="flex flex-col col-start-1 col-end-3 row-start-3 row-end-4 gap-4 w-full md:col-start-2 md:row-start-2 md:row-end-3">
