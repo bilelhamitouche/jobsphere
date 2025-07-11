af5c5ea23b193b6c58ef971eb52cc194840d7df5
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-11 19:08:48 +0100

    add not found page

---
+++ app/recruiter/resumes/[resumeId]/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex justify-center items-center p-4 w-full h-full">
+      <p className="text-xl text-gray-400">No Resume found</p>
+    </div>
+  );
+}
