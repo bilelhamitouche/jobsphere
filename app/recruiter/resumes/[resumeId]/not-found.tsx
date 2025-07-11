ff2a6715099570460094c36cd84de9af4b35fcf7
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-11 23:38:14 +0100

    fix styling

--- app/recruiter/resumes/[resumeId]/not-found.tsx
+++ app/recruiter/resumes/[resumeId]/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
-    <div className="flex justify-center items-center p-4 w-full h-full">
-      <p className="text-xl text-gray-400">No Resume found</p>
+    <div className="flex justify-center items-center p-8 w-full h-full">
+      <p className="text-xl text-gray-700">No Resume found</p>
     </div>
   );
 }
