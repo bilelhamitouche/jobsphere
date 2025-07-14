d053645680b084503661dbe83e4151eae773d58a
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-07-14 02:40:50 +0100

    add schemas for changing password and account info

--- lib/zod.ts
+++ lib/zod.ts
@@ -108,3 +108,26 @@ export const updateCompanyInfoSchema = z.object({
   industry: companyIndustry.optional(),
   logo_url: z.string().trim().optional(),
 });
+
+export const passwordChangeSchema = z.object({
+  password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+  new_password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+  confirm_new_password: z
+    .string()
+    .trim()
+    .min(8, { message: "Must be 8 characters long" })
+    .max(20, { message: "Must be at most 20 characters long" }),
+});
+
+export const accountChangeSchema = z.object({
+  name: z.string().trim().min(1, { message: "Name is required" }),
+  email: z.string().trim().email({ message: "Must be a valid Email address" }),
+});
