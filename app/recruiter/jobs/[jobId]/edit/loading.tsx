dc3463931ed315d78c84756471335c172d812ccf
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-11-26 01:09:17 +0100

    feat: add loading spinner for job edit form

---
+++ app/recruiter/jobs/[jobId]/edit/loading.tsx
@@ -0,0 +1,12 @@
+import { Loader } from "lucide-react";
+
+export default function Loading() {
+  return (
+    <div className="p-6 space-y-4 w-full h-full">
+      <h2 className="text-3xl font-bold">Update Job</h2>
+      <div className="flex justify-center items-center w-full h-full">
+        <Loader className="animate-spin" size="40" />
+      </div>
+    </div>
+  );
+}
