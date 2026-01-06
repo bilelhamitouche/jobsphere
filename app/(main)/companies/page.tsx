7df2a8c565a87914ed3f088d78f28b70dc1bc35e
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-01-06 20:40:55 +0100

    styles: add grid style and gaps

--- app/(main)/companies/page.tsx
+++ app/(main)/companies/page.tsx
@@ -1,18 +1,15 @@
 import CompaniesList from "./components/CompaniesList";
+import Filter from "./components/Filter";
 import Search from "./components/Search";
 
-export default async function Companies({
-  searchParams,
-}: {
-  searchParams: Promise<{ search?: string }>;
-}) {
-  const { search } = await searchParams;
+export default async function Companies() {
   return (
-    <div className="p-8 space-y-8 bg-primary-foreground">
+    <div className="p-8 space-y-8 h-full bg-primary-foreground">
       <h2 className="text-3xl font-bold">Browse Companies</h2>
-      <div className="space-y-8">
+      <div className="grid gap-4 grid-cols-[auto_1fr] grid-rows-[auto_auto_1fr]">
         <Search />
-        <CompaniesList search={search || ""} />
+        <Filter />
+        <CompaniesList />
       </div>
     </div>
   );
