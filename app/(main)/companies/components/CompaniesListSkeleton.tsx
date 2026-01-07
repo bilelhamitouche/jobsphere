7401ef746cfd809b3416c02e14db804d569970ba
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-01-07 06:39:15 +0100

    styles: change from 3 columns to 2 columns on large screens

--- app/(main)/companies/components/CompaniesListSkeleton.tsx
+++ app/(main)/companies/components/CompaniesListSkeleton.tsx
@@ -3,7 +3,7 @@ import CompanySkeleton from "./CompanySkeleton";
 
 export default function CompaniesListSkeleton() {
   return (
-    <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+    <ul className="grid gap-4 lg:grid-cols-2">
       {Array.from({ length: COMPANY_LIMIT }, () => null).map((_, index) => (
         <CompanySkeleton key={index} />
       ))}
