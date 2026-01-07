bd9b33c0b971e0bfe81095eeccf9e73e33e7db70
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2026-01-07 06:39:42 +0100

    fix: wrap search and filter in fragment to avoid useSearchParams issue

---
+++ app/(main)/companies/components/Controls.tsx
@@ -0,0 +1,11 @@
+import Filter from "./Filter";
+import Search from "./Search";
+
+export default function Controls() {
+  return (
+    <>
+      <Search />
+      <Filter />
+    </>
+  );
+}
--- app/(main)/companies/page.tsx
+++ app/(main)/companies/page.tsx
@@ -1,16 +1,14 @@
 import { Suspense } from "react";
 import CompaniesList from "./components/CompaniesList";
-import Filter from "./components/Filter";
-import Search from "./components/Search";
 import CompaniesListSkeleton from "./components/CompaniesListSkeleton";
+import Controls from "./components/Controls";
 
 export default async function Companies() {
   return (
     <div className="p-8 space-y-8 h-full bg-primary-foreground">
       <h2 className="text-2xl font-bold">Browse Companies</h2>
       <div className="grid gap-4 grid-cols-[auto_1fr] grid-rows-[auto_auto_1fr]">
-        <Search />
-        <Filter />
+        <Controls />
         <Suspense fallback={<CompaniesListSkeleton />}>
           <CompaniesList />
         </Suspense>
